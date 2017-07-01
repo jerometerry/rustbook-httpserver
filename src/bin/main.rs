2254e99de35ad0f59bfc12ce9187ff0547eac935
Jerome Terry <jerome.terry@verafin.com>
2017-07-01 16:56:12 -0230

    Thread Pool Implementation

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,14 +2,21 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::fs::File;
+extern crate httpserver;
+use httpserver::ThreadPool;
+
 use std::thread;
 use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let pool = ThreadPool::new(4);
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
